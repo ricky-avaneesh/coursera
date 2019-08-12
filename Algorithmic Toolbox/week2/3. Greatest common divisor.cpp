8fcf588a8719b8d8cfbed6726bda3568d8dcc6b4
ricky-avaneesh <ricky-avaneesh>
2019-08-12 14:45:22 +0530

    Finding Greatest Common Divisor of two numbers

--- Algorithmic Toolbox/week2/3. Greatest common divisor.cpp
+++ Algorithmic Toolbox/week2/3. Greatest common divisor.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+using namespace std;
 int gcd_naive(int a, int b) {
   int current_gcd = 1;
   for (int d = 2; d <= a && d <= b; d++) {
@@ -12,9 +12,25 @@ int gcd_naive(int a, int b) {
   return current_gcd;
 }
 
+long int gcd_fast(int a, int b)
+{
+	while(1)
+	{
+		if(a<b)
+		{
+			a += b;
+			b = a-b;
+			a -= b;
+		}
+		a = a-b;
+		if(a==b)
+			return a;
+	}
+}
 int main() {
   int a, b;
-  std::cin >> a >> b;
-  std::cout << gcd_naive(a, b) << std::endl;
+  cin >> a >> b;
+  //cout << gcd_naive(a, b) <<endl;
+  cout<<gcd_fast(a,b)<<endl;
   return 0;
 }
