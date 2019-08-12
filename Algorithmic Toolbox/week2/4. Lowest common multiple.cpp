2f7f583aed91fc8f94b4e6d081ea05ba97983f76
ricky-avaneesh <ricky-avaneesh>
2019-08-12 15:08:00 +0530

    Finding LCM of two numbers

--- Algorithmic Toolbox/week2/4. Lowest common multiple.cpp
+++ Algorithmic Toolbox/week2/4. Lowest common multiple.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+using namespace std;
 long long lcm_naive(int a, int b) {
   for (long l = 1; l <= (long long) a * b; ++l)
     if (l % a == 0 && l % b == 0)
@@ -8,9 +8,40 @@ long long lcm_naive(int a, int b) {
   return (long long) a * b;
 }
 
+long long int lcm_fast(int a, int b)
+{
+	int gcd;
+	long long int lcm;
+	int c,d;
+	c=a;
+	d=b;
+	while(1)
+	{
+		if(a<b)
+		{
+			a += b;
+			b = a-b;
+			a -= b;
+		}
+		a = a-b;
+		if(a==b)
+		{
+			gcd = a;
+			break;
+		}
+	}
+
+	lcm = (c*d)/gcd;
+	return lcm;
+}
+
 int main() {
-  int a, b;
-  std::cin >> a >> b;
-  std::cout << lcm_naive(a, b) << std::endl;
+  int a, b,gcd;
+  unsigned long long int lcm;
+  cin >> a >> b;
+  //cout << lcm_naive(a, b)<<endl;
+  cout<<lcm_fast(a,b)<<endl;
+  //gcd = gcd_fast(a,b);
+  //cout<<gcd<<endl;
   return 0;
 }
