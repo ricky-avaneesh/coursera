3f8c5d15f64beae5466d62c072911bb0885a24d2
ricky-avaneesh <ricky-avaneesh>
2019-08-12 14:42:37 +0530

    Printing last digit of Nth term of Fibonacci Sequence

--- Algorithmic Toolbox/week2/2. Fibonacci last digit.cpp
+++ Algorithmic Toolbox/week2/2. Fibonacci last digit.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+using namespace std;
 int get_fibonacci_last_digit_naive(int n) {
     if (n <= 1)
         return n;
@@ -15,10 +15,30 @@ int get_fibonacci_last_digit_naive(int n) {
 
     return current % 10;
 }
+int get_fibonacci_last_digit_fast(int n)
+{
+	if(n<=1)
+		return n;
 
+	int pre = 0;
+	int cur = 1;
+	int tmp_pre;
+	for(int i=0; i<n-1; ++i)
+	{	tmp_pre = pre;
+		pre = cur;
+		cur = tmp_pre + cur;
+		if(tmp_pre>9)
+			tmp_pre %= 10;
+		if(cur>9)
+			cur %= 10;
+		if(pre>9)
+			pre %= 10;
+	}
+	return cur;
+}
 int main() {
     int n;
-    std::cin >> n;
-    int c = get_fibonacci_last_digit_naive(n);
-    std::cout << c << '\n';
+    cin >> n;
+    int c = get_fibonacci_last_digit_fast(n);
+    cout << c << '\n';
     }
