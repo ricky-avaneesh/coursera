45b2ed8982a13733e08d68f46a090b6ed8761441
ricky-avaneesh <avaneeshmaithil@gmail.com>
2019-08-16 15:48:09 +0530

    F(n) modulo m using pissano period

--- Algorithmic Toolbox/week2/5. fibonacci huge.cpp
+++ Algorithmic Toolbox/week2/5. fibonacci huge.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+using namespace std;
 long long get_fibonacci_huge_naive(long long n, long long m) {
     if (n <= 1)
         return n;
@@ -10,14 +10,34 @@ long long get_fibonacci_huge_naive(long long n, long long m) {
     for (long long i = 0; i < n - 1; ++i) {
         long long tmp_previous = previous;
         previous = current;
-        current = tmp_previous + current;
+        current = (tmp_previous + current)%m;
     }
 
-    return current % m;
+    return current;
 }
 
+long long int pissano_period(int m)
+{
+	long long int a =0, b=1, c = a+b;
+	for(int i=0; i<m*m; i++)
+	{
+		c = (a+b)%m;
+		a = b;
+		b = c;
+		if(a==0 && b==1)
+			return(i+1);
+	}
+}
 int main() {
     long long n, m;
-    std::cin >> n >> m;
-    std::cout << get_fibonacci_huge_naive(n, m) << '\n';
+    long long int per;
+    int new_fibbonacci;
+    cin >> n >> m;
+    per = pissano_period(m);
+	//cout<<"\nperiod = "<<per<<endl;
+    new_fibbonacci = n % per;
+    if(new_fibbonacci == 0)
+        new_fibbonacci = per;
+	//cout<<"new nth fib number = "<<new_fibbonacci<<endl;
+    cout << get_fibonacci_huge_naive(new_fibbonacci, m) << '\n';
 }
