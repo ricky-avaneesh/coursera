137b762d5cd0bfab51e336451788c1fcbd74e9b8
ricky-avaneesh <avaneeshmaithil@gmail.com>
2019-08-19 13:36:29 +0530

    Last Digit of the Sum of Fibonacci Numbers Within An Interval

--- Algorithmic Toolbox/week2/7. Fibonacci partial sum.cpp
+++ Algorithmic Toolbox/week2/7. Fibonacci partial sum.cpp
@@ -1,28 +1,47 @@
 #include <iostream>
 #include <vector>
-using std::vector;
-
-long long get_fibonacci_partial_sum_naive(long long from, long long to) {
-    long long sum = 0;
+using namespace  std;
+int fibonassi_period()
+{
+	int current=0, next=1;
+	int c  = (current + next);
+	for(int i=0; i<100; ++i)
+	{
+		c = (current + next)%10;
+		current = next;
+		next = c;
+		if(current==0 && next==1)
+			return(i+1);
+	}
+}
+int get_fibonacci_partial_sum_naive(long long from, long long to) {
+    int sum = 0;
 
     long long current = 0;
     long long next  = 1;
-
+    long long new_current;
     for (long long i = 0; i <= to; ++i) {
         if (i >= from) {
-            sum += current;
+            sum += (current % 10);
+            sum %= 10;
         }
 
-        long long new_current = next;
-        next = next + current;
-        current = new_current;
+        new_current = next%10;
+        next = (next + current) % 10;
+        current = new_current % 10;
     }
 
-    return sum % 10;
+    return sum;
 }
 
 int main() {
     long long from, to;
-    std::cin >> from >> to;
-    std::cout << get_fibonacci_partial_sum_naive(from, to) << '\n';
+
+    int per = fibonassi_period();
+    //cout<<"Value of variable per : "<<per<<endl;
+    long long n = to - from;
+    int rem = n % per;
+    to = from + rem;
+    cin >> from >> to;
+    cout << get_fibonacci_partial_sum_naive(from, to) << '\n';
 }
